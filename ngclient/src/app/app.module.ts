95312c155980285ecd59b3ad4e8ee5978d6451ee
JeHaluska <je.haluska@strike-design.net>
2022-03-23 09:23:50 +0100

    Import form module

--- ngclient/src/app/app.module.ts
+++ ngclient/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
@@ -19,6 +20,7 @@ import { ContactAddComponent } from './contact-add/contact-add.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(
       [
         { path: "all", component: ContactListComponent},
